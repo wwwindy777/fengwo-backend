430e5d262420f9f416fc7a519c3e994508d77968
wwwwind <1429395174@qq.com>
2023-03-06 15:12:14 +0800

    修改配置bug，配置docker file

--- src/main/java/com/mulan/fengwo_backend/config/RedissonConfig.java
+++ src/main/java/com/mulan/fengwo_backend/config/RedissonConfig.java
@@ -14,13 +14,14 @@ import org.springframework.context.annotation.Configuration;
 public class RedissonConfig {
     private String host;
     private String port;
+    private String password;
 
     @Bean
     public RedissonClient redissonClient() {
         // 1. 创建配置（官网的配置useClusterServers()是分布式集群redis）
         String address = String.format("redis://%s:%s", host, port);
         Config config = new Config();
-        config.useSingleServer().setAddress(address).setDatabase(3);
+        config.useSingleServer().setAddress(address).setPassword(password).setDatabase(3);
         // 2. 创建对象
         return Redisson.create(config);
     }
